f89d55d03c9a564c5499457840daba2adced7bb7
Aaron Branch <aaron.d.branch@wmich.edu>
2016-03-01 21:11:42 -0500

    updated cunit and unit test for outputformatting

--- Src/FormattingOutput/t1.c
+++ Src/FormattingOutput/t1.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "outputformatting.h"
+#include "cunit.h"
 
 int main(){
+	/*Declare variables*/
+	char *sret = (char *)malloc(1024);
+
 	/*Test complex root output*/
-	formatoutput(0, 1, 2, 3, 25, 25);
+	sret = formatoutput(0, 1, 2, 3, 25, 25);
+	assert_streq("Error validating root output", sret, "");
+	
 
 	/*Test double root output*/
-	formatoutput(1, 1, 2, 3, 25, 25);
+	sret = formatoutput(1, 1, 2, 3, 25, 25);
+	assert_streq("Error validating root output", sret, "");
 
 	/*Test 2 roots output*/
-	formatoutput(2, 1, 2, 3, 25, 25);
+	sret = formatoutput(2, 1, 2, 3, 25, 25);
+	assert_streq("Error validating root output", sret, "");
+
+	return 0;
 }
